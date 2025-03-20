90b969a13bdd3e382e545c6561824bb7b9a426cc
Adriano Rovelli <arovelli.dev@gmail.com>
2025-03-20 17:38:16 -0300

    Proyecto biblioteca para el segundo trabajo practico de programacion avanzada esta finalizado

---
+++ TP2_ProgramacionAvanzada/src/Biblioteca/Libro.java
@@ -0,0 +1,45 @@
+package Biblioteca;
+
+public class Libro {
+    private String titulo, autor, tipo;
+    private int año;
+
+    public Libro(String titulo, String autor, String tipo, int año) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.tipo = tipo;
+        this.año = año;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+}
